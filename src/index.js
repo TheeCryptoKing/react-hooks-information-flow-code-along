930eb1fb2a99703f283f1298e603ce027f5ff3cd
kwamebrowne <theeecryptoking@gmail.com>
2023-05-02 22:04:25 -0500

    finished code with minor bug copy and pasted though

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// When the div in the Child component is clicked, it will use the onChangeColor variable to determine what function to run
+// onChangeColor is a prop that is passed down from the Parent component, which references the handleChangeColor function
+// The handleChangeColor function is the function that will actually run when the div is clicked, and will update state in the Parent component
+
+// Sibling components cannot pass data to each other directly
+// Data can only flow up and down between parent and child
+
